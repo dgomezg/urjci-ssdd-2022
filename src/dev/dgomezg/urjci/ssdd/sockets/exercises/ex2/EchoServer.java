331552d12e5e61e8ed605e0b3c89c6555b8664ed
dgomezg <dgomezg@gmail.com>
2022-01-30 13:15:26 +0100

    Ex2. EchoServer implemented

--- src/dev/dgomezg/urjci/ssdd/sockets/exercises/ex2/EchoServer.java
+++ src/dev/dgomezg/urjci/ssdd/sockets/exercises/ex2/EchoServer.java
@@ -1,13 +1,15 @@
 package dev.dgomezg.urjci.ssdd.sockets.exercises.ex2;
 
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
 
     public static final int PORT = 8080;
+    public static final String EOT_COMMAND = "x";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //DONE: Open a ServerSocket listening on PORT 8080 (or any other available port)
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server Listening on " + serverSocket.getLocalSocketAddress());
@@ -18,12 +20,22 @@ public class EchoServer {
                 processRequest(serviceSocket);
             }
         } //DONE: Don't forget to close the resources (Sockets...)
-
-
     }
 
-    private static void processRequest(Socket serviceSocket) {
-        //TODO: Process the request received through serviceSocket
-        //DONE: Don't forget to close the resources (Sockets, InputStreams...)
+    private static void processRequest(Socket serviceSocket) throws IOException {
+        try (   InputStream inputStream = serviceSocket.getInputStream();
+                BufferedReader socketReader =
+                        new BufferedReader(new InputStreamReader(inputStream));
+                OutputStream outputStream = serviceSocket.getOutputStream();
+                PrintWriter socketWriter = new PrintWriter(outputStream, true)) {
+
+            String line = null;
+            while ( (line = socketReader.readLine()) != null
+                     && !line.equals(EOT_COMMAND)) {
+                System.out.println("< " + line);
+                socketWriter.println("> " + line);
+            }
+
+        } // Autocloseable resources implicitly closed.
     }
 }
