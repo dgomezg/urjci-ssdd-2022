4e01253b3844de251f616c8200b28047ef86a382
dgomezg <dgomezg@gmail.com>
2022-01-30 11:55:00 +0100

    Step 2: Server properly closes Sockets

--- src/dev/dgomezg/urjci/ssdd/sockets/samples/DummyServer.java
+++ src/dev/dgomezg/urjci/ssdd/sockets/samples/DummyServer.java
@@ -9,13 +9,31 @@ public class DummyServer {
     public static final int PORT = 8080;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.println("Server Listening on  "
-                + serverSocket.getLocalSocketAddress());
+        ServerSocket serverSocket = null;
+        Socket serviceSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server Listening on  "
+                    + serverSocket.getLocalSocketAddress());
 
-        Socket serviceSocket = serverSocket.accept();
-        System.out.println("Service Socket for request open on port "
-                + serviceSocket.getPort());
-
+            serviceSocket = serverSocket.accept();
+            System.out.println("Service Socket for request open on port "
+                    + serviceSocket.getPort());
+        } finally {
+            if (serviceSocket != null) {
+                try {
+                    serviceSocket.close();
+                } catch (Exception ex) {
+                    // close() can throw an exception and
+                    // even if we are not going to handle it,
+                    // we need to catch it to allow the finally block
+                    // to properly close the serverSocket
+                }
+            }
+            if (serverSocket != null) {
+                //No need to catch Exception, as this is the last resource
+                serverSocket.close();
+            }
+        }
     }
 }
