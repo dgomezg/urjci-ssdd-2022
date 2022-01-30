ab40a338f0da7b11ff964c60daf519390129e36a
dgomezg <dgomezg@gmail.com>
2022-01-30 12:02:07 +0100

    Step 3: Autocloseable Sockets (implicitly) closed with try-with-resources

--- src/dev/dgomezg/urjci/ssdd/sockets/samples/DummyServer.java
+++ src/dev/dgomezg/urjci/ssdd/sockets/samples/DummyServer.java
@@ -9,31 +9,15 @@ public class DummyServer {
     public static final int PORT = 8080;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = null;
-        Socket serviceSocket = null;
-        try {
-            serverSocket = new ServerSocket(PORT);
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server Listening on  "
                     + serverSocket.getLocalSocketAddress());
 
-            serviceSocket = serverSocket.accept();
-            System.out.println("Service Socket for request open on port "
-                    + serviceSocket.getPort());
-        } finally {
-            if (serviceSocket != null) {
-                try {
-                    serviceSocket.close();
-                } catch (Exception ex) {
-                    // close() can throw an exception and
-                    // even if we are not going to handle it,
-                    // we need to catch it to allow the finally block
-                    // to properly close the serverSocket
-                }
-            }
-            if (serverSocket != null) {
-                //No need to catch Exception, as this is the last resource
-                serverSocket.close();
-            }
-        }
+            try (Socket serviceSocket = serverSocket.accept()) {
+                System.out.println("Service Socket for request open on port "
+                        + serviceSocket.getPort());
+            } // serviceSocket (Autocloseable) is properly closed by try-with-resources
+        } // serverSocket (Autocloseable) is properly closed by try-with-resources
     }
 }
