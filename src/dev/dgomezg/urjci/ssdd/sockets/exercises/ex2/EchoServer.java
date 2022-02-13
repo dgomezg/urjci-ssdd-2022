adce28f7ee8f9b17197c9cbfc38aaf88eae7a8db
dgomezg <dgomezg@gmail.com>
2022-02-13 10:48:50 +0100

    Ex2. Step 1. EchoServer Created and accepting client connection

--- src/dev/dgomezg/urjci/ssdd/sockets/exercises/ex2/EchoServer.java
+++ src/dev/dgomezg/urjci/ssdd/sockets/exercises/ex2/EchoServer.java
@@ -1,11 +1,29 @@
 package dev.dgomezg.urjci.ssdd.sockets.exercises.ex2;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
 
+    public static final int PORT = 8080;
+
     public static void main(String[] args) {
-        //TODO: Open a ServerSocket listening on PORT 8080 (or any other available port)
-        //TODO: Accept connections from serverSocket and get serviceSocket
+        //DONE: Open a ServerSocket listening on PORT 8080 (or any other available port)
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server Listening on " + serverSocket.getLocalSocketAddress());
+
+            //DONE: Accept connections from serverSocket and get serviceSocket
+            try (Socket serviceSocket = serverSocket.accept()) {
+                System.out.println("Accepted connection on port " + serviceSocket.getPort());
+                processRequest(serviceSocket);
+            }
+        } //DONE: Don't forget to close the resources (Sockets...)
+
+
+    }
+
+    private static void processRequest(Socket serviceSocket) {
         //TODO: Process the request received through serviceSocket
-        //TODO: Don't forget to close the resources (Sockets, Input/OutputStreams....
+        //DONE: Don't forget to close the resources (Sockets, InputStreams...)
     }
 }
