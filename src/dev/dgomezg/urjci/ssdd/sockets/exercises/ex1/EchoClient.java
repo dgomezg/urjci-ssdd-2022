e205b4b7906712db9e792aef987a30dc286052ad
dgomezg <dgomezg@gmail.com>
2022-02-13 10:59:29 +0100

    Ex1 Step 1: Echo client connects to server

--- src/dev/dgomezg/urjci/ssdd/sockets/exercises/ex1/EchoClient.java
+++ src/dev/dgomezg/urjci/ssdd/sockets/exercises/ex1/EchoClient.java
@@ -1,11 +1,22 @@
 package dev.dgomezg.urjci.ssdd.sockets.exercises.ex1;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class EchoClient {
 
-    public static void main(String[] args) {
-        //TODO: create a socket connection to the server
-        //TODO: Read a line from standard input and send it to Server
-        //TODO: wait for server response and print on the standard output
-        //TODO: Don't forget to close the resources (Sockets, Input/OutputStreams)
+    public static final String SERVER_HOST = "localhost";
+    public static final int PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        //DONE: create a socket connection to the server
+        try (Socket socket = new Socket(SERVER_HOST, PORT)) {
+            System.out.println("Connected to server on " + socket.getRemoteSocketAddress());
+
+            //TODO: Read a line from standard input and send it to Server
+            //TODO: wait for server response and print on the standard output
+            //TODO: Don't forget to close the resources (Input/OutputStreams)
+
+        } //Socket (Autocloseable) closed implicitly
     }
 }
