527501cbbdd13434c4c83b765cb64627abfbe700
dgomezg <dgomezg@gmail.com>
2022-03-12 21:02:51 +0100

    Ex 6.2.1: Step 2a - Added logging to Greeting Controller

--- 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/GreetingController.java
+++ 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/GreetingController.java
@@ -1,5 +1,7 @@
 package es.urjc.ssdd.dgomezg.springboot.samplemvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GreetingController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GreetingController.class);
+
     @GetMapping("/greeting")
     public String greeting(Model model) {
+        LOGGER.debug("Invoking Greeting controller");
         model.addAttribute("name", "David");
         return "greeting_template";
     }
