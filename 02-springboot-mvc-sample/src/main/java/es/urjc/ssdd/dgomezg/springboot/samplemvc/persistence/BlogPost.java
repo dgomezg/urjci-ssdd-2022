1b2ba49832c919cfd64f32945030502e97f8b193
dgomezg <dgomezg@gmail.com>
2022-03-13 12:29:29 +0100

    Ex 6.3.2: Step 2a - Add setters to the Entity so that Spring can fill the values from the Request

--- 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/persistence/BlogPost.java
+++ 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/persistence/BlogPost.java
@@ -31,4 +31,50 @@ public class BlogPost {
         this.publishDate = publishDate;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getPublishDate() {
+        return publishDate;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setPublishDate(LocalDateTime publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    @Override
+    public String toString() {
+        return "BlogPost{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", publishDate=" + publishDate +
+                '}';
+    }
 }
