c3be9fbf70bccba97cab3a767b692751afba2036
dgomezg <dgomezg@gmail.com>
2022-03-13 12:29:00 +0100

    Ex 6.3.2: Step 2 - Add Controller to handle the POST

--- 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/BlogPostController.java
+++ 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/BlogPostController.java
@@ -2,23 +2,39 @@ package es.urjc.ssdd.dgomezg.springboot.samplemvc;
 
 import es.urjc.ssdd.dgomezg.springboot.samplemvc.persistence.BlogPost;
 import es.urjc.ssdd.dgomezg.springboot.samplemvc.persistence.BlogPostRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
 public class BlogPostController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BlogPostController.class);
+
     @Autowired
     private BlogPostRepository repository;
 
-    @GetMapping("blog")
+    @GetMapping("/blog")
     public String list(Model model) {
         List<BlogPost> blogPosts = repository.findAll();
         model.addAttribute("blogPosts", blogPosts);
         return "blog/index";
+    }
+
+    @PostMapping("/blog/save-new-post")
+    public String saveBlogPost(BlogPost blogPost) {
+        LOGGER.trace("Received Blog post from {} with title {}", blogPost.getAuthor(), blogPost.getTitle());
+        blogPost.setPublishDate(LocalDateTime.now());
+
+        repository.save(blogPost);
+        LOGGER.info("New Blog post {} from {} saved with id {}", blogPost.getTitle(), blogPost.getAuthor(), blogPost.getId());
+
+        return "redirect:/blog";
     }
 }
