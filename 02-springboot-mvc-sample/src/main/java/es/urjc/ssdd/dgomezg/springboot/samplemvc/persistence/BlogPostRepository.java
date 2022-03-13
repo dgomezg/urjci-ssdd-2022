4ff1b1e1e8a819e983f0a3e160d00a0cd2aea433
dgomezg <dgomezg@gmail.com>
2022-03-13 09:44:20 +0100

    Exercise 6.3.1: Step 3a - Added aditional find methods to Repository

--- 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/persistence/BlogPostRepository.java
+++ 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/persistence/BlogPostRepository.java
@@ -2,6 +2,12 @@ package es.urjc.ssdd.dgomezg.springboot.samplemvc.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+
+    List<BlogPost> findByAuthor(String author);
+
+    List<BlogPost> findByTitle(String title);
 
 }
