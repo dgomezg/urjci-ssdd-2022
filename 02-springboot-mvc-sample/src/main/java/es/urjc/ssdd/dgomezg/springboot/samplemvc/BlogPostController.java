7c412811ea15e96063fe436a5b6c8e0b92f6c04d
dgomezg <dgomezg@gmail.com>
2022-03-13 12:48:08 +0100

    Ex 6.3.2: Step 3 (optional) - POST redirects (POST-REDIRECT-GET) specifically to new Blog post content view.

--- 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/BlogPostController.java
+++ 02-springboot-mvc-sample/src/main/java/es/urjc/ssdd/dgomezg/springboot/samplemvc/BlogPostController.java
@@ -5,11 +5,15 @@ import es.urjc.ssdd.dgomezg.springboot.samplemvc.persistence.BlogPostRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Controller
@@ -28,13 +32,28 @@ public class BlogPostController {
     }
 
     @PostMapping("/blog/save-new-post")
-    public String saveBlogPost(BlogPost blogPost) {
+    public ResponseEntity<Object> saveBlogPost(BlogPost blogPost) {
         LOGGER.trace("Received Blog post from {} with title {}", blogPost.getAuthor(), blogPost.getTitle());
         blogPost.setPublishDate(LocalDateTime.now());
 
         repository.save(blogPost);
         LOGGER.info("New Blog post {} from {} saved with id {}", blogPost.getTitle(), blogPost.getAuthor(), blogPost.getId());
 
-        return "redirect:/blog";
+        //return "redirect:/blog";
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, "/blog/post/" + blogPost.getId())
+                .build();
+    }
+
+    @GetMapping("/blog/post/{blogPostId}")
+    public String view(Model model, @PathVariable Long blogPostId) {
+        BlogPost blogPost = repository.getById(blogPostId);
+        model.addAttribute("blogPost", blogPost);
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        String publishDateFormatted = blogPost.getPublishDate().format(dateTimeFormatter);
+        model.addAttribute("publishDateFormatted", publishDateFormatted);
+
+        return "blog/view-blog-post";
     }
 }
